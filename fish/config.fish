9cde060fc5e9080be641cd13f9b6e8a2b0579cdd
Rushil Mehra <rushilmhr@gmail.com>
2022-06-10 11:53:38 -0700

    Minimal baseline for dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+
+# vim keybinds
+fish_hybrid_key_bindings
+
+# aliases
+alias vim="nvim"
+alias ls="ls -laG"
